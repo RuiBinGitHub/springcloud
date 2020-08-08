e1bdae0aab681ef5b883b7ec7dca97500231f875
甘瑞彬 <2770471187@qq.com>
2020-08-08 17:22:17 +0800

    2020-8-8 17:20:02

--- springcloud-api/src/test/java/com/example/AppMainTests.java
+++ springcloud-api/src/test/java/com/example/AppMainTests.java
@@ -3,7 +3,7 @@ package com.example;
 public class AppMainTests {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello word");
     }
 
 }
