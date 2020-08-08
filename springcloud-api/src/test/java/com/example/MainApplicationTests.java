76b1eb4fb9f8d5b063bc1ca8048f3e0d1949f0dd
甘瑞彬 <2770471187@qq.com>
2020-08-08 18:01:57 +0800

    2020-8-8 18:01:05

--- springcloud-api/src/test/java/com/example/MainApplicationTests.java
+++ springcloud-api/src/test/java/com/example/MainApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MainApplicationTests {
+public class MainApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
