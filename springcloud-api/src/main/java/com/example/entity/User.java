a3d979773e1d730cad3e420387590eae2feee9b9
甘瑞彬 <2770471187@qq.com>
2020-08-08 16:54:25 +0800

    2020-8-8 16:54:10

---
+++ springcloud-api/src/main/java/com/example/entity/User.java
@@ -0,0 +1,54 @@
+package com.example.entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = -2248190721476487645L;
+
+    private int id;
+    private String name;
+    private String username;
+    private String password;
+    private String date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
