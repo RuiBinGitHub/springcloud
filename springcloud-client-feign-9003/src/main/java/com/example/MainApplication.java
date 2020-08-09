770e7a439beec4709a521dc2cccfd89af0b4b111
甘瑞彬 <2770471187@qq.com>
2020-08-09 15:59:08 +0800

    2020-8-9 15:57:54

---
+++ springcloud-client-feign-9003/src/main/java/com/example/MainApplication.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableEurekaClient
+@EnableFeignClients
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
